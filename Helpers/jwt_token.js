c079c1512c569a8fbfc389ae94bd89e084682431
kouri <KouririZohayr>
2024-10-26 19:07:12 +0100

    create token for register update

--- Helpers/jwt_token.js
+++ Helpers/jwt_token.js
@@ -5,7 +5,7 @@ module.exports = {
     signAcesseToken : (userId) => {
         return new Promise((resolve, reject) => {
             const payload = {};  // removed `aud` from here
-            const secret = 'some super secret';
+            const secret =  process.env.ACCESS_TOKEN_SECRET ;
             const options = {
                 expiresIn: '1h',
                 audience: userId  // define audience here
