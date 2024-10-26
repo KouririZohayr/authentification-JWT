37aead69a807c755cc82ea750f87319e11ba3b01
kouri <KouririZohayr>
2024-10-26 18:41:14 +0100

    create token for register update

--- Helpers/jwt_token.js
+++ Helpers/jwt_token.js
@@ -1,21 +1,22 @@
 const JWT = require("jsonwebtoken");
-const creatError = require('http-errors');
+const createError = require('http-errors');
 
 module.exports = {
-    signAcesseToken : (userId)=>{
-        return new Promise((resolve, reject)=>{
-            const payload = {
-                name : 'zohayr'
-            }
-            const secret = 'some super secret'
+    signAcesseToken : (userId) => {
+        return new Promise((resolve, reject) => {
+            const payload = {};  // removed `aud` from here
+            const secret = 'some super secret';
             const options = {
-
-            }
-            JWT.sign(payload , secret , options , (err , token)=>{
-                if (err) reject(err)
-                    resolve(token)
-                console.log(token)
-            })
-        })
+                expiresIn: '1h',
+                audience: userId  // define audience here
+            };
+            JWT.sign(payload, secret, options, (err, token) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(token);
+                console.log(token);
+            });
+        });
     }
-}
+};
